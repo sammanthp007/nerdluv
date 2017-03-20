8a1a0c81828813c979b40fb00ab1fb2a76067b77
sammanthp007 <samman.thapa@bison.howard.edu>
2017-03-20 17:29:17 -0400

    add css to signup

--- signup.php
+++ signup.php
@@ -1,63 +1,29 @@
 <?php include("top.html"); ?>
 
 <div>
-    <h1>New User Signup:</h1>
-
-<table>
     <form action="signup-submit.php" method="post">
-    <tr>
-        <td>Name:</td> 
-        <td>
-            <input type="text" name="name" size="17" maxlength="16"> 
-        </td>
-    </tr>
-    <tr>
-        <td>Gender:</td>
-        <td>
+    <fieldset>
+    <legend>New User Signup:</legend>
+    <strong class="column">Name:</strong>
+<input type="text" name="name" size="17" maxlength="16"><br><br>
+    <strong class="column">Gender:</strong>
             <input type="radio" name="gender" value="M">Male
-            <input type="radio" name="gender" value="F" checked>Female
-        </td>
-    </tr>
-    <tr>
-        <td>Age:</td>
-        <td>
-            <input type="text" name="age" size="6" maxlength="2">
-        </td>
-    </tr>
-    <tr>
-        <td>Personality type:</td>
-        <td>
-            <input type="text" name="personality_type" size="6" maxlength="4">
-        </td>
-    </tr>
-    <tr>
-        <td>
-            Favorite OS:
-        </td>
-        <td>
+            <input type="radio" name="gender" value="F" checked>Female<br><br>
+    <strong class="column">Age:</strong><input type="text" name="age" size="6" maxlength="2"><br><br>
+    <strong class="column">Personality Type:</strong><input type="text" name="personality_type" size="6" maxlength="4"><br><br>
+
+            <strong class="column">Favorite OS:</strong>
             <select name="os">
                 <option value="Linux">Linux</option>
                 <option value="Mac OS X" >Mac OS X</option>
                 <option value="Windows"selected>Windows</option>
-            </select><br>
-        </td>
-    </tr>
-    <tr>
-        <td>
-            Seeking age:
-        </td>
-        <td>
+            </select><br><br>
+            <strong class="column">Seeking age:</strong>
             <input type="text" name="min_seeking_age" size="6" maxlength="2" placeholder="min"> to
-            <input type="text" name="max_seeking_age" size="6" maxlength="2" placeholder="max">
-        </td>
-    </tr>
-    <tr>
-        <td>
-            <input type="submit" value="Sign Up">
-        </td>
-    </tr>
+            <input type="text" name="max_seeking_age" size="6" maxlength="2" placeholder="max"><br><br>
+            
+<input type="submit" value="Sign Up">
     </form>
-</table>
 </div>
 
 <?php include("bottom.html"); ?>
