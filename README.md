7df6a26cf3f249e40dce470fecee71151d924c77
sammanthp007 <samman.thapa@bison.howard.edu>
2017-03-19 23:41:45 -0400

    completed signup.php

---
+++ README.md
@@ -0,0 +1,22 @@
+- [x] Name: A 16-character box for the user to type a name. A
+name should only have alphabetic letters with the first letter
+of each world capitalized.
+- [x] Gender: Radio buttons for the user to select a gender of Male
+or Female. When the user clicks the text next to a radio button,
+that button should become checked. Initially Female is
+checked.
+- [x] Age: A 6-letter-wide text input box for the user to type his/her
+age in years. The box should allow typing up to 2 characters.
+The characters must be digits only.
+- [x] Personality type: A 6-character-wide text box allowing the
+user to type a Keirsey personality type, such as ISTJ or ENFP.
+The box should let the user type up to 4 characters. The label
+has a link to http://www.humanmetrics.com/cgi-win/JTypes2.asp . 
+The input must be one of the 16 types.
+- [x] Favorite OS: A drop-down select box allowing the user to
+select a favorite operating system. 
+The choices are Windows, Mac OS X, and Linux. Initially "Windows" is selected.
+- [x] Seeking age: Two 6-character-wide text boxes for the user to specify the range of acceptable ages of partners. The box should allow the user to type up to 2 characters in each box. 
+Initially both are empty and have placeholder text of "min"
+and "max" respectively. When the user starts typing, this placeholder text disappears. The input must be digits only.
+- [x] Sign Up: When pressed, submits the form for processing as described below.
--- support_files/signup.php
+++ support_files/signup.php
@@ -1,26 +1,64 @@
 <?php include("top.html"); ?>
 
-<!-- Web Programming Step by Step, Homework 4 (NerdLuv)
-     This provided file is the front page that links to two of the files you are going
-     to write, signup.php and matches.php.  You don't need to modify this file. -->
 <div>
-	<h1>Welcome!</h1>
+    <h1>New User Signup:</h1>
 
-	<ul>
-		<li>
-			<a href="signup.php">
-				<img src="signup.gif" alt="icon" />
-				Sign up for a ne
-			</a>
-		</li>
-		
-		<li>
-			<a href="matches.php">
-				<img src="heartbig.gif" alt="icon" />
-				Check your matches
-			</a>
-		</li>
-	</ul>
+<table>
+    <form action="/signup-submit.php" method="post">
+<tr>
+        <td>Name:</td> <td><input type="text" name="name" size="17" maxlength="16"> </td>
+</tr>
+<tr>
+        <td>Gender:</td>
+<td>
+            <input type="radio" name="gender" value="M">Male
+            <input type="radio" name="gender" value="F" checked>Female
+</td>
+</tr>
+<tr>
+<td>
+Age:
+</td>
+<td>
+<input type="text" name="age" size="6" maxlength="2">
+</td>
+</tr>
+<tr>
+<td>
+        Personality type:
+</td>
+<td>
+<input type="text" name="personality_type" size="6" maxlength="4">
+</td>
+</tr>
+<tr>
+<td>
+        Favorite OS:
+</td>
+<td>
+            <select name="os">
+                <option value="Linux">Linux</option>
+                <option value="Mac OS X" >Mac OS X</option>
+                <option value="Windows"selected>Windows</option>
+            </select><br>
+</td>
+</tr>
+<tr>
+<td>
+        Seeking age:
+</td>
+<td>
+            <input type="text" name="min_seek_age" size="6" maxlength="2" placeholder="min"> to
+            <input type="text" name="max_seek_age" size="6" maxlength="2" placeholder="max">
+</td>
+</tr>
+<tr>
+<td>
+        <input type="submit" value="Sign Up">
+</td>
+</tr>
+    </form>
+</table>
 </div>
 
 <?php include("bottom.html"); ?>
